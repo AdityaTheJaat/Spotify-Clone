5601f4609f499084e53ab2a5dfa93e6531007b00
AdityaTheJaat <programmingID1306@gamil.com>
2023-08-09 22:00:33 +0530

    .

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { signup, login, logout } = require('../controllers/Auth')
+const passport = require('passport')
 
 router.post('/register', signup)
 router.post('/login', login)
-router.post('/logout', logout)
+router.post('/logout', passport.authenticate("jwt", {session:false}), logout)
 
 module.exports = router
